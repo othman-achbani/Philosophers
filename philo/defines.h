89515fc7d30175e61a2de894e9b58ff1ff77e9d8
othman-achbani <othmanachbani61@gmail.com>
2025-05-21 17:40:00 +0100

    finishing the parsing

--- philo/defines.h
+++ philo/defines.h
@@ -6,7 +6,7 @@
 /*   By: oachbani <oachbani@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/21 11:42:24 by oachbani          #+#    #+#             */
-/*   Updated: 2025/05/21 11:57:50 by oachbani         ###   ########.fr       */
+/*   Updated: 2025/05/21 17:35:02 by oachbani         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,5 +15,8 @@
 
 #define ARG_NUMBER_ERR "Error: Invalid number of arguments. try again\n"
 #define NOT_VALID  "Error: the number is not valid try another one\n"
+#define NEGATIVE "Error : negative numbers is invalid\n"
+#define NOT_LOGIC "Error : please use a logic number and try again\n"
+
 
 #endif
--- philo/philo.c
+++ philo/philo.c
@@ -6,7 +6,7 @@
 /*   By: oachbani <oachbani@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/21 11:24:27 by oachbani          #+#    #+#             */
-/*   Updated: 2025/05/21 12:35:22 by oachbani         ###   ########.fr       */
+/*   Updated: 2025/05/21 17:39:23 by oachbani         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,22 +16,46 @@ void    check_arg(char *str)
 {
     int i;
 
-    i = 0;
-    if (str[i] == '-' || str[i] == '+')
-        str + 1;
-    while (str[i])
+    i = -1;
+    if (str[0] == '-' || str[0] == '+')
     {
+        if (str[0] == '-')
+        {
+            printf(NEGATIVE);
+            exit(EXIT_FAILURE);
+        }
+        str += 1;
+    }
+    while (str[++i])
         if (!ft_isdigit(str[i]))
         {
             printf(NOT_VALID);
             exit(EXIT_FAILURE);
-        }       
+        }
+    if (ft_atoi(str) > INT_MAX || ft_atoi(str) == 0)
+    {
+        printf(NOT_LOGIC);
+        exit(EXIT_FAILURE);
     }
+}
+
+t_philo fill_struct(char **av)
+{
+    t_philo philo;
+
+    philo.nb_philo = ft_atoi(av[1]);
+    philo.time_to_die = ft_atoi(av[2]);
+    philo.time_to_eat = ft_atoi(av[3]);
+    philo.time_to_sleep= ft_atoi(av[4]);
+    if (av[5])
+        philo.nb_eat_time= ft_atoi(av[5]);
+    return(philo);
 }
 
 t_philo ft_parsing(int  ac, char **av)
 {
     int i;
+    t_philo philo;
 
     i = 0;
     if (ac != 5 && ac != 6)
@@ -41,11 +65,22 @@ t_philo ft_parsing(int  ac, char **av)
     }
     while (av[++i])
         check_arg(av[i]);
-    
+    philo = fill_struct(av);
+    return(philo);   
 }
 
 
 int main (int ac, char **av)
 {
-    ft_parsing(ac, av);
+    t_philo ph_data;
+
+    ph_data = ft_parsing(ac, av);
+    printf("the number of philos is %d\n", ph_data.nb_philo);
+    printf("the time to die is %d\n", ph_data.time_to_die);
+    printf("the time to eat is %d\n", ph_data.time_to_eat);
+    printf("the time to sleep is %d\n", ph_data.time_to_sleep);
+    printf("the number of times each philo should eat is %d\n ", ph_data.nb_eat_time);
+        
+        
+
 }
